9a85069a2e338f621cc4fdab5565ac421808be93
justin.fu <1162942285@qq.com>
2025-03-21 21:57:06 +0800

    哈希

--- 04_hash_table/2.tow_sum.py
+++ 04_hash_table/2.tow_sum.py
@@ -18,4 +18,66 @@ def two_sum(nums, target):
         if complement in hash_map:
             return [hash_map[complement], i]  # 找到结果
         hash_map[num] = i  # 将当前数值及其索引存入哈希表
+        print(hash_map)
+
     return []
+
+
+
+import unittest
+
+
+class TestTwoSum(unittest.TestCase):
+    def test_two_sum_FoundPair_ReturnsIndices(self):
+        nums = [2, 7, 11, 15]
+        target = 9
+        expected = [0, 1]
+        result = two_sum(nums, target)
+        self.assertEqual(result, expected)
+
+
+    def test_two_sum_MultiplePairs_ReturnsFirstPair(self):
+        nums = [3, 3, 3, 3]
+        target = 6
+        expected = [0, 1]
+        result = two_sum(nums, target)
+        self.assertEqual(result, expected)
+
+    def test_two_sum_SingleElement_ReturnsEmptyList(self):
+        nums = [1]
+        target = 1
+        expected = []
+        result = two_sum(nums, target)
+        self.assertEqual(result, expected)
+
+    def test_two_sum_EmptyList_ReturnsEmptyList(self):
+        nums = []
+        target = 1
+        expected = []
+        result = two_sum(nums, target)
+        self.assertEqual(result, expected)
+
+    def test_two_sum_NegativeNumbers_ReturnsIndices(self):
+        nums = [-1, -2, -3, -4]
+        target = -6
+        expected = [1, 3]
+        result = two_sum(nums, target)
+        self.assertEqual(result, expected)
+
+    def test_two_sum_ZeroTarget_ReturnsIndices(self):
+        nums = [0, 0, 1, 2]
+        target = 0
+        expected = [0, 1]
+        result = two_sum(nums, target)
+        self.assertEqual(result, expected)
+
+    def test_two_sum_LargeNumbers_ReturnsIndices(self):
+        nums = [1000000000, 1000000000]
+        target = 2000000000
+        expected = [0, 1]
+        result = two_sum(nums, target)
+        self.assertEqual(result, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
